3535cf1f5e6671a6cb8e4654d3e6805d6891f09e
AlexCawl <babushkinsusu@gmail.com>
2023-06-28 23:05:44 +0500

    added gradle-wrapper folder to git

--- app/src/main/java/org/alexcawl/todoapp/domain/util/DuplicationException.kt
+++
@@ -1,3 +0,0 @@
-package org.alexcawl.todoapp.domain.util
-
-class DuplicationException(message: String): RuntimeException(message)
--- app/src/main/java/org/alexcawl/todoapp/domain/util/IllegalUUIDException.kt
+++
@@ -1,3 +0,0 @@
-package org.alexcawl.todoapp.domain.util
-
-class IllegalUUIDException(message: String): RuntimeException(message)
